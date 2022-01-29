dc09b368f7b84904d10406cb1306bc6091ebe065
ajayjayaraj559 <ajayjayaraj559@gmail.com>
2022-01-30 00:23:53 +0530

    hjhj

--- lib/Maternity/Others/newsdata.dart
+++ lib/Maternity/Others/newsdata.dart
@@ -1,24 +1,24 @@
 final NewsList = [
   {
-    "name": "Covid Rises",
-    "type": "Over 100000 deaths",
+    "name": "Healthy Eating",
+    "type": "Eat a new vegetable you've never tried.",
     "image":"https://image.freepik.com/free-vector/doctor-character-background_1270-84.jpg",
     "description": "A doctor can be found in several settings, including public health organization, group practices and  hospitals They have some of the most diverse and challenging careears available and part of a universally well-respected profession",
   },
   {
-    "name": "4-in-1",
-    "type": "A mother gives birth to 4 children",
+    "name": "Quit smoking",
+    "type": "There are many programs to help you.",
     "image":"https://image.freepik.com/free-vector/doctor-character-background_1270-84.jpg",
     "description": "A doctor can be found in several settings, including public health organization, group practices and  hospitals They have some of the most diverse and challenging careears available and part of a universally well-respected profession",
   },
   {
-    "name": "News3",
+    "name": "Rest when you can. Nap!",
     "type": "",
     "image":"https://image.freepik.com/free-vector/doctor-character-background_1270-84.jpg",
     "description": "A doctor can be found in several settings, including public health organization, group practices and  hospitals They have some of the most diverse and challenging careears available and part of a universally well-respected profession",
   },
   {
-    "name": "News 4",
+    "name": "Baby sit a friend's baby and learn a bit about caring for a newborn.",
     "type": "",
     "image":"https://image.freepik.com/free-vector/doctor-character-background_1270-84.jpg",
     "description": "A doctor can be found in several settings, including public health organization, group practices and  hospitals They have some of the most diverse and challenging careears available and part of a universally well-respected profession",
