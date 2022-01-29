88c0d0cf322669a03e65aeffcab563862866d621
ajayjayaraj559 <ajayjayaraj559@gmail.com>
2022-01-29 21:47:10 +0530

    color

--- lib/Maternity/maternity.dart
+++ lib/Maternity/maternity.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:med_devs/Doctor/appodoc.dart';
-// import 'package:med_devs/Maternity/Bmi/screens/input_page.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:med_devs/Maternity/tools/counter.dart';
 import 'package:med_devs/Style/extension.dart';
-// import 'package:med_devs/Style/lcolor.dart';
 import 'package:med_devs/Style/textst.dart';
 import 'package:med_devs/Style/theme.dart';
 
 class Maternity extends StatefulWidget {
-  const Maternity({ Key? key }) : super(key: key);
+  const Maternity({Key? key}) : super(key: key);
 
   @override
   _MaternityState createState() => _MaternityState();
@@ -19,7 +17,7 @@ class _MaternityState extends State<Maternity> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey[100],
+        backgroundColor: Colors.grey[200],
         resizeToAvoidBottomInset: false,
         body: GestureDetector(
           onTap: () {
@@ -49,87 +47,86 @@ class _MaternityState extends State<Maternity> {
                     margin: EdgeInsets.only(top: 300),
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(20),
-                      color: Colors.blueGrey[100],
+                      color: Colors.grey[200],
                     ),
                     child: Padding(
-                      padding: EdgeInsets.all(23),
+                      padding: EdgeInsets.all(10),
                       child: Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.only(
-                          top: 8, right: 16, left: 16, bottom: 4),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: <Widget>[
-                          Text("Category", style: TextStyles.title.bold),
-                          Text(
-                            "See All",
-                            style: TextStyles.titleNormal.copyWith(
-                                color: Theme.of(context).primaryColor),
-                          ).p(8).ripple(() {})
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                top: 8, right: 16, left: 16, bottom: 4),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Text("Category", style: TextStyles.title.bold),
+                                Text(
+                                  "See All",
+                                  style: TextStyles.titleNormal.copyWith(
+                                      color: Theme.of(context).primaryColor),
+                                ).p(8).ripple(() {})
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: AppTheme.fullHeight(context) * .28,
+                            width: AppTheme.fullWidth(context),
+                            child: ListView(
+                              scrollDirection: Axis.horizontal,
+                              children: <Widget>[
+                                GestureDetector(
+                                  child: _categoryCard(
+                                      "Baby Kicker",
+                                      "3 days left ...",
+                                      "https://media.istockphoto.com/photos/closeup-detail-of-two-adorable-little-baby-feet-picture-id157281452?k=20&m=157281452&s=170667a&w=0&h=WvfWyS6fGKf-usbRNNlZgSnPx6HXoTfCVp49Kepqcio=",
+                                      color: Colors.green,
+                                      lightColor: Colors.greenAccent),
+                                  onDoubleTap: () => Navigator.of(context).push(
+                                      MaterialPageRoute(
+                                          builder: (context) => Counter())),
+                                  onLongPress: () => Navigator.of(context).push(
+                                      MaterialPageRoute(
+                                          builder: (context) => Counter())),
+                                ),
+                                GestureDetector(
+                                  child: _categoryCard("", "3 days left ...",
+                                      "https://thumbs.dreamstime.com/b/bmi-body-mass-index-calculator-icon-illustration-vector-bmi-body-mass-index-calculator-icon-illustration-157560815.jpg",
+                                      color: Colors.green,
+                                      lightColor: Colors.greenAccent),
+                                  onLongPress: () {
+                                    print("ewed");
+                                    _launchURL();
+                                  },
+                                  onDoubleTap: () {
+                                    setState(() {
+                                      print("fdff");
+                                      _launchURL();
+                                    });
+                                  },
+                                ),
+                                GestureDetector(
+                                  child: _categoryCard("", "50 + Doctors",
+                                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy1vYq5hVWmM-oOUPrrZFo9QbaEKyhn01ZZfXxhin_77HDR6URtD6lbb4iRxJWshgTLYY&usqp=CAU",
+                                      color: Colors.orange,
+                                      lightColor: Colors.orange),
+                                  onDoubleTap: () => Navigator.of(context).push(
+                                      MaterialPageRoute(
+                                          builder: (context) => DocAPPo())),
+                                  onLongPress: () => Navigator.of(context).push(
+                                      MaterialPageRoute(
+                                          builder: (context) => DocAPPo())),
+                                ),
+                                GestureDetector(
+                                  child: _categoryCard("", "899 Doctors",
+                                      "https://cdn-icons-png.flaticon.com/512/3158/3158990.png",
+                                      color: Colors.blue,
+                                      lightColor: Colors.lightBlueAccent),
+                                ),
+                              ],
+                            ),
+                          ),
                         ],
                       ),
-                    ),
-                    SizedBox(
-                      height: AppTheme.fullHeight(context) * .28,
-                      width: AppTheme.fullWidth(context),
-                      child: ListView(
-                        scrollDirection: Axis.horizontal,
-                        children: <Widget>[
-                          GestureDetector(
-                            child: _categoryCard(
-                                "Baby Kicker",
-                                "3 days left ...",
-                                "https://media.istockphoto.com/photos/closeup-detail-of-two-adorable-little-baby-feet-picture-id157281452?k=20&m=157281452&s=170667a&w=0&h=WvfWyS6fGKf-usbRNNlZgSnPx6HXoTfCVp49Kepqcio=",
-                                color: Colors.green,
-                                lightColor: Colors.greenAccent),
-                                onDoubleTap: () => Navigator.of(context).push(MaterialPageRoute(
-                                    builder: (context) => Counter())),
-                                    onLongPress: () =>  Navigator.of(context).push(MaterialPageRoute(
-                                    builder: (context) => Counter())),
-                          ),
-                          GestureDetector(
-                            child: _categoryCard(
-                                "",
-                                "3 days left ...",
-                                "https://thumbs.dreamstime.com/b/bmi-body-mass-index-calculator-icon-illustration-vector-bmi-body-mass-index-calculator-icon-illustration-157560815.jpg",
-                                color: Colors.green,
-                                lightColor: Colors.greenAccent),
-                               onLongPress: ()
-                                {
-                                  print("ewed");
-                                  _launchURL();
-                                },
-                            onDoubleTap: () {
-                              setState(() {
-                                print("fdff");
-                                _launchURL();
-                              });
-                            },
-                          ),
-                          GestureDetector(
-                            child: _categoryCard(
-                                "",
-                                "50 + Doctors",
-                                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy1vYq5hVWmM-oOUPrrZFo9QbaEKyhn01ZZfXxhin_77HDR6URtD6lbb4iRxJWshgTLYY&usqp=CAU",
-                                color: Colors.orange,
-                                lightColor: Colors.orange),
-                                onDoubleTap: () => Navigator.of(context).push(MaterialPageRoute(
-                                    builder: (context) => DocAPPo())),
-                                    onLongPress: () =>  Navigator.of(context).push(MaterialPageRoute(
-                                    builder: (context) => DocAPPo())),
-                          ),
-                           GestureDetector(
-                            child: _categoryCard("", "899 Doctors",
-                                "https://cdn-icons-png.flaticon.com/512/3158/3158990.png",
-                                color: Colors.blue,
-                                lightColor: Colors.lightBlueAccent),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
                     ),
                   )
                 ],
@@ -138,7 +135,8 @@ class _MaternityState extends State<Maternity> {
           ),
         ));
   }
-   Widget _categoryCard(String title, String subtitle, String link,
+
+  Widget _categoryCard(String title, String subtitle, String link,
       {Color? color, required Color lightColor}) {
     TextStyle titleStyle = TextStyles.title.bold.white;
     TextStyle subtitleStyle = TextStyles.body.bold.white;
@@ -147,7 +145,7 @@ class _MaternityState extends State<Maternity> {
       subtitleStyle = TextStyles.bodySm.bold.white;
     }
     return AspectRatio(
-      aspectRatio: 12 / 14,
+      aspectRatio: 6 / 8,
       child: Container(
         // height: 280,
         width: AppTheme.fullWidth(context) * .3,
@@ -187,7 +185,14 @@ class _MaternityState extends State<Maternity> {
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
                     Flexible(
-                      child: Text(title,style: TextStyle(color: Colors.white,fontSize: 25,fontWeight: FontWeight.bold),textAlign: TextAlign.center,),
+                      child: Text(
+                        title,
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center,
+                      ),
                     ),
                     // SizedBox(height: 10),
                     // Flexible(
@@ -205,9 +210,11 @@ class _MaternityState extends State<Maternity> {
       ),
     );
   }
+
   _launchURL() async {
     print("ererge");
-    const url = 'https://www.calculator.net/bmi-calculator.html?ctype=metric&cage=25&csex=m&cheightfeet=5&cheightinch=10&cpound=160&cheightmeter=180&ckg=65&printit=0';
+    const url =
+        'https://www.calculator.net/bmi-calculator.html?ctype=metric&cage=25&csex=m&cheightfeet=5&cheightinch=10&cpound=160&cheightmeter=180&ckg=65&printit=0';
     if (await canLaunch(url)) {
       await launch(url, forceWebView: true, enableJavaScript: true);
     } else {
